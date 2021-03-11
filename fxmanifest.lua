be2644124835b6cb795e9f1a58876044a709fedb
Christopher <onlygamernerd@gmail.com>
2021-03-11 21:02:45 +0100

    fixed spawning horse

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,6 @@ fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-client_script 'dist/client/*.client.js'
+client_script 'resources/dist/client/*.client.js'
 
-server_script 'dist/server/*.server.js'
+server_script 'resources/dist/server/*.server.js'
--- resources/client/cl_commands.ts
+++ resources/client/cl_commands.ts
@@ -1,23 +1,19 @@
 import { Delay } from '../utils/fivem';
-
-
-async function spawnHorse() {
-  const model = GetHashKey('A_C_Horse_AmericanPaint_Greyovero');
+ 
+async function spawnHorse(horseName: string) {
+  const model = GetHashKey(horseName);
   const pos = GetEntityCoords(PlayerPedId(), true);
-  const horse = CreateVehicle(model, pos[0], pos[1], pos[2], 100.0, true, true);
+  const horse: string = Citizen.invokeNative('0xD49F9B0955C367DE', model, pos[0] + 2, pos[1], pos[2], 100.0, true, true, true, true);
 
-  Citizen.invokeNative('0xD49F9B0955C367DE', model, pos[0], pos[1], pos[2], 100.0, true, true, true, true);
+  RequestModel(horse);
 
-  RequestModel(model);
-
-  while (!HasModelLoaded(model)) {
+  while (!HasModelLoaded(horse)) {
     await Delay(100);
   }
 
   Citizen.invokeNative('0x283978A15512B2FE', horse, true);
 }
 
-RegisterCommand('spawnhorse', async () => {
-  console.log('hello there')
-  await spawnHorse()
+RegisterCommand('spawnhorse', async (source: number, args: string[], raw: string) => {
+  await spawnHorse(args[1])
 }, false);
